ecbbc35593f286b65c54052204f545c0594961b0
harish chaudhary <harishchaudhary@harishs-MacBook-Air.local>
2023-11-16 22:57:43 +0530

    updated code

--- socket/socketIoHandler.js
+++ socket/socketIoHandler.js
@@ -1,25 +1,17 @@
-const productSchema = require("../models/product.models")
+const productSchema = require("../models/product.models");
 
 const socketHandler = (io) => {
   io.on("connection", async (socket) => {
     console.log("A user connected");
 
-    io.emit("allData", await productSchema.find({}));
-
-    socket.on("join trade", tradeId => {
-      console.log('tradeId', tradeId)
-      socket.join(tradeId);
-
-      socket.on("filterDataGet", async (data) => {
-        console.log('data', data)
-        setInterval(async () => {
-          socket.emit(
-            "filterDataSend",
-            await productSchema.find({ name: { $in: data.identifier } })
-          );
-        }, 1000);
-      });
-    })
+    socket.on("filterDataGet", async (data) => {
+      setInterval(async () => {
+        socket.emit(
+          "filterDataSend",
+          await productSchema.find({ name: { $in: data.identifier } })
+        );
+      }, 1000);
+    });
 
     // Set up a disconnect event
     socket.on("disconnect", () => {
